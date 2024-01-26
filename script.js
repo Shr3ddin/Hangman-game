05e1a627b05bc8dbf3a4a95b9ccf18c67a4152d1
Shr3ddin <patrykprzybylek94@gmail.com>
2024-01-26 01:38:58 +0100

    working on logic - start game, losing/wining conditions

--- script.js
+++ script.js
@@ -44,13 +44,19 @@ const keyboard = document.querySelector('.keyboard');
 const keys = keyboard.querySelectorAll('.key');
 const hintContent = document.querySelector('.hint-text');
 const wordLetters = document.querySelector('.word-letters');
+const guesses = document.querySelector('.guesses-text span');
+const hangmanImg = document.querySelector('.hangman-img-box img');
+const modal = document.querySelector('.result-modal');
 
 let currentWord;
+let mistake = 0;
+let correctLetters = [];
+const maxAttempts = 5;
 
 const getRandomExample = () => {
 	const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
 
-	currentWord = word;
+	currentWord = word.toUpperCase();
 
 	hintContent.innerHTML = `<p><span>Hint:</span> ${hint}</p>`;
 
@@ -60,14 +66,38 @@ const getRandomExample = () => {
 		.join('');
 };
 
+const gameOver = isWin => {
+	setTimeout(() => {
+		const modalText = isWin ? `You guessed the word correctly!` : `Correct answer was:`;
+		modal.querySelector('h2').textContent = `${isWin ? 'Congrats! You won!' : 'Game Over! You lost!'}`;
+		modal.querySelector('p').innerHTML = `${modalText} <span>${currentWord}</span>`;
+		modal.classList.add('active');
+	}, 200);
+};
+
 const startGame = e => {
 	const clickedKey = e.target.textContent;
 
+	console.log(currentWord);
+
 	if (currentWord.includes(clickedKey)) {
-		console.log(clickedKey, 'exist on the list');
+		[...currentWord].forEach((letter, index) => {
+			if (letter === clickedKey) {
+				correctLetters.push(letter);
+				wordLetters.querySelectorAll('p')[index].textContent = letter;
+				wordLetters.querySelectorAll('p')[index].classList.add('guessed');
+			}
+		});
 	} else {
-		console.log(clickedKey);
+		mistake++;
+		hangmanImg.src = `./img/hangman-${mistake}.svg`;
 	}
+
+	e.target.disabled = 'true';
+	guesses.textContent = `${mistake} / ${maxAttempts}`;
+
+	if (maxAttempts === mistake) return gameOver(false);
+	if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
 keys.forEach(key => {
